08ead060a17800a40adf277f8aaadd129f248a1a
Kedar Nagekar <kedarnagekar@gmail.com>
2026-04-06 13:48:19 +0530

    1. Updated git ignore file and other changes.

---
+++ Places/Group/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  Places
+//
+//  Created by Kedar Nagekar on 06/04/26.
+//
+
+import Foundation
---
+++ Places/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Places
+//
+//  Created by Kedar Nagekar on 06/04/26.
+//
+
+import Foundation
